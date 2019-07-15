e14e637ec30a06e7d5ab6781e271adc8304406ef
Hinata10 <shinkai.xzettai.62@icloud.com>
2019-07-16 03:43:06 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 日向亮博 on 2019/07/16.
+//  Copyright © 2019 Hinata10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
